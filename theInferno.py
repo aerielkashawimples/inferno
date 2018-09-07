b0024abef9c1704dc4b84741bc3eafe127d0939a
aerielkashawimples <43015108+aerielkashawimples@users.noreply.github.com>
2018-09-06 20:04:42 -0400

    This is the final test. Be prepared.

---
+++ theInferno.py
@@ -0,0 +1,119 @@
+questionOneCorrect = 0
+print("Welcome to Hell.") 
+print("First question.")
+while questionOneCorrect == 0:
+    print("How many levels of hell are there in Dante's Inferno?")
+    questionOneAnswer = input()
+    questionOneAnswer = str(questionOneAnswer)
+    if str.lower(questionOneAnswer) == "nine":
+        print("Correct!")
+        questionOneCorrect = 1
+    elif questionOneAnswer == "9":
+        print("Correct!")
+        questionOneCorrect = 1
+    else:
+        print("Incorrect!")
+questionTwoCorrect = 0
+print("Second question.")
+while questionTwoCorrect == 0:
+    print("To which sin is the second circle of Hell dedicated?")
+    questionTwoAnswer = input()
+    questionTwoAnswer = str(questionTwoAnswer)
+    if str.lower(questionTwoAnswer) == "lust":
+        print("Correct!")
+        questionTwoCorrect = 1
+    else:
+        print("Incorrect!")
+questionThreeCorrect = 0
+print("Third question.")
+while questionThreeCorrect == 0:
+    print("At level eight, what famous figure did they meet?")
+    questionThreeAnswer = input()
+    questionThreeAnswer = str(questionThreeAnswer)
+    if str.lower(questionThreeAnswer) == "pope nicholas iii":
+        print("Correct!")
+        questionThreeCorrect = 1
+    elif str.lower(questionThreeAnswer) == "pope nicholas the third":
+        print("Correct!")
+        questionThreeCorrect = 1
+    else:
+        print("Incorrect!")
+print("Fourth question.")
+questionFourCorrect = 0
+while questionFourCorrect == 0:
+    print("In what year was the first Dante's Inferno movie released?")
+    questionFourAnswer = input()
+    questionFourAnswer = str(questionFourAnswer)
+    if questionFourAnswer == "1911":
+        print("Correct!")
+        questionFourCorrect = 1
+    else:
+        print("Incorrect!")
+print("Fifth question. Getting hot in here!")
+questionFiveCorrect = 0
+while questionFiveCorrect == 0:
+    print("Who guides Dante through the circles of hell?")
+    questionFiveAnswer = input()
+    if str.lower(questionFiveAnswer) == "virgil":
+        print("Correct!")
+        questionFiveCorrect = 1
+    else:
+        print("Incorrect!")
+print("Sixth question.")
+questionSixCorrect = 0
+while questionSixCorrect == 0:
+    print("What is Virgil's occupation?")
+    questionSixAnswer = input()
+    if str.lower(questionSixAnswer) == "poet":
+        print("Correct!")
+        questionSixCorrect = 1
+    elif str.lower(questionSixAnswer) == "a poet":
+        print("Correct!")
+        questionSixCorrect = 1
+    else:
+        print("Incorrect!")
+print("Seventh question")
+questionSevenCorrect = 0
+while questionSevenCorrect == 0:
+    print("Dante was an italian, so he was biased against ------, hence their gods in each level")
+    questionSevenAnswer = input()
+    if str.lower(questionSevenAnswer) == "greeks":
+        print("Correct!")
+        questionSevenCorrect = 1
+    elif str.lower(questionSevenAnswer) == "greek people":
+        print("Correct!")
+        questionSevenCorrect = 1
+    else:
+        print("Incorrect!")
+print("Eighth qustion")
+questionEightCorrect = 0
+while questionEightCorrect == 0:
+    print("In Paradiso, what place is defined as the abode of God?")
+    questionEightAnswer = input()
+    if str.lower(questionEightAnswer) == "empyrean":
+        print("Correct!")
+        questionEightCorrect = 1
+    elif str. lower(questionEightAnswer) == "the empyrean":
+        print("Correct!")
+        questionEightCorrect = 1
+    else:
+        print("Incorrect!")
+print("Ninth question")
+questionNineCorrect = 0
+while questionNineCorrect == 0:
+    print("Throughout The Divine Comedy, Dante is led by Virgil through the levels of hell, purgatory, and then up to Heaven.  Throughout this journey, they met important figures all the way up to the big man himself; God.  He speaks with guardians and sinners alike and many either represent or tell a moral lesson that we can walk away with.  One of these is not to let your morals be overcome by anger, gluttony, and greed.  Dante is the representation of man's ___ and its need to repent.")
+    questionNineAnswer = input()
+    if str.lower(questionNineAnswer) == "sin":
+          print("Correct!")
+          questionNineCorrect = 1
+    else:
+          print("Incorrect!")
+
+for count in range(100):
+          print("")
+print("Congratulations and Thank You for playing! Hopefully you have learned much on this journey, or at least had some fun.  Take away from this game that patience is a virtue and the rememberance to spend your time wisely~! For consolidated completion, please send your regards to youdidityoureanerd@gmail.com ;)")
+
+
+          
+                            
+
