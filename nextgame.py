c064f6bee292e71cdc9001e3acbf0b3d62d5a4e0
aerielkashawimples <43015108+aerielkashawimples@users.noreply.github.com>
2018-11-05 10:14:50 -0500

    Add files via upload

---
+++ nextgame.py
@@ -0,0 +1,112 @@
+questionOneCorrect = 0
+print("Welcome back. let's test your memory.") 
+print("First question.")
+while questionOneCorrect == 0:
+    print("How many puzzles were there in the original game?")
+    questionOneAnswer = raw_input()
+    questionOneAnswer = str(questionOneAnswer)
+    if str.lower(questionOneAnswer) == "four":
+        print("Correct!")
+        questionOneCorrect = 1
+    elif questionOneAnswer == "4":
+        print("Correct!")
+        questionOneCorrect = 1
+    else:
+        print("Incorrect!")
+questionTwoCorrect = 0
+print("Second question.")
+while questionTwoCorrect == 0:
+    print("Who was the famous figure you put in the anagram generator?")
+    questionTwoAnswer = raw_input()
+    questionTwoAnswer = str(questionTwoAnswer)
+    if str.lower(questionTwoAnswer) == "william shakespeare":
+        print("Correct!")
+        questionTwoCorrect = 1
+    else:
+        print("Incorrect!")
+questionThreeCorrect = 0
+print("Third question.")
+while questionThreeCorrect == 0:
+    print("What are we?")
+    questionThreeAnswer = raw_input()
+    questionThreeAnswer = str(questionThreeAnswer)
+    if str.lower(questionThreeAnswer) == "the eye":
+        print("Correct!")
+        questionThreeCorrect = 1
+    else:
+        print("Incorrect!")
+print("Fourth question.")
+questionFourCorrect = 0
+while questionFourCorrect == 0:
+    print("What social media cite did you use?")
+    questionFourAnswer = raw_input()
+    questionFourAnswer = str(questionFourAnswer)
+    if str.lower(questionFourAnswer) == "instagram":
+        print("Correct!")
+        questionFourCorrect = 1
+    else:
+        print("Incorrect!")
+print("Fifth question. Are you sure you want to continue?")
+questionFiveCorrect = 0
+while questionFiveCorrect == 0:
+    print("You are no longer safe... in the last game what was the quiz about?")
+    questionFiveAnswer = raw_input()
+    questionFiveAnswer = str(questionFiveAnswer)
+    if str.lower(questionFiveAnswer) == "dantes inferno":
+        print("Correct!")
+        questionFiveCorrect = 1
+    else:
+        print("Incorrect!")
+print("Sixth question.")
+questionSixCorrect = 0
+while questionSixCorrect == 0:
+    print("Do you beleive in hell?")
+    questionSixAnswer = raw_input()
+    if str.lower(questionSixAnswer) == "yes":
+        print("Correct!")
+        questionSixCorrect = 1
+    elif str.lower(questionSixAnswer) == "of course":
+        print("Correct!")
+        questionSixCorrect = 1
+    else:
+        print("Incorrect!")
+print("Seventh question")
+questionSevenCorrect = 0
+while questionSevenCorrect == 0:
+    print("You have gone too far, there is no turning back. Do not blame us for what is to come. Are you ready?")
+    questionSevenAnswer = raw_input()
+    if str.lower(questionSevenAnswer) == "yes":
+        print("Correct!")
+        questionSevenCorrect = 1
+    elif str.lower(questionSevenAnswer) == "of course":
+        print("Correct!")
+        questionSevenCorrect = 1
+    else:
+        print("You may leave now")
+print("Eighth qustion")
+questionEightCorrect = 0
+while questionEightCorrect == 0:
+    print("Im glad you stayed, it would be no fun if you left.... What's Our name")
+    questionEightAnswer = raw_input()
+    if str.lower(questionEightAnswer) == "beta":
+        print("Correct!")
+        questionEightCorrect = 1
+    elif str. lower(questionEightAnswer) == "beta2332":
+        print("Correct!")
+        questionEightCorrect = 1
+    else:
+        print("Incorrect!")
+print("Ninth question")
+questionNineCorrect = 0
+while questionNineCorrect == 0:
+    print("Your mother and father have been kidnapped. The person says,'pick one, and the other dies.' What do you say?")
+    questionNineAnswer = raw_input()
+    if str.lower(questionNineAnswer) == "":
+          print("Correct!")
+          questionNineCorrect = 1
+    else:
+          print("Incorrect!")
+
+for count in range(100):
+          print("")
+print("Congratulations and Thank You for playing! Hopefully you have learned much on this journey, or at least had some fun.  Take away from this game that patience is a virtue and the rememberance to spend your time wisely~! For consolidated completion, please send your regards to youdidityoureanerd@gmail.com ;)")
